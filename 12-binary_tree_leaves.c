81463e9dcfae1db708dafec2cc43926a948eff1c
Abdoulkarim <amahamn@gmail.com>
2022-08-27 01:02:37 +0200

    final

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -5,21 +5,16 @@
  * If tree is NULL, return 0
  * Return: the leaves
  */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaf = 0;
-	size_t value_b = 0;
-	size_t value_a = 0;
+	size_t leaves = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		value_a = binary_tree_leaves(tree->right);
-		value_b = binary_tree_leaves(tree->left);
-		leaf = value_b + value_a;
-		return ((!value_b && !value_a) ? leaf + 1 : leaf + 0);
-	}
+	if (!tree->left && !tree->right)
+		leaves += 1;
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+	return (leaves);
 }
