aa2e6036e9712611abf78c040221e8762ee56f7d
Abdoulkarim <amahamn@gmail.com>
2022-08-27 01:22:33 +0200

    final

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,14 +5,12 @@
  * @tree: a pointer to the root node of the tree to delete
  * Return: Nothing
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
-	else if (tree == NULL){}
-
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
