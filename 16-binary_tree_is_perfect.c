bc7da271807964d4b701396ca63025529c1c126d
Abdoulkarim <amahamn@gmail.com>
2022-08-27 02:06:14 +0200

    final

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -60,7 +60,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_size = 0;
 	size_t right_size = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	left_size += 1 + binary_tree_height(tree->left);
